bc254a5de0ec3a15fd830ec524032a91f5245de9
BB9z <bb9z@me.com>
2024-01-19 22:21:35 +0800

    Real build tests.

---
+++ tests/samples/BuildTests/HasWarning/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  BuildTests
+//
+//  Copyright (c) 2024 BB9z, MIT License
+//
+
+import Foundation
+
+"unused"
+
+if (nil == 1) {
+}
---
+++ tests/samples/BuildTests/NoWarning/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BuildTests
+//
+//  Copyright (c) 2024 BB9z, MIT License
+//
+
+import Foundation
+
+print("Hello, World!")
+
